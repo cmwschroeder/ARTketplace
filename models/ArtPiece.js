6c352b3bc129ad86392d01f4bea5e34453d6c4cd
M A Razvi <abuzarflw@gmail.com>
2022-05-09 13:50:48 -0700

    Define ArtPiece Model amd its fields

---
+++ models/ArtPiece.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class ArtPiece extends Model {}
+
+ArtPiece.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: "user",
+          key: "id"
+      }
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    image: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    collection_id : {
+        type: DataTypes.INTEGER,
+        references: {
+            model: "collection",
+            key: "id"
+        }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'artPiece',
+  }
+);
+
+module.exports = ArtPiece;
