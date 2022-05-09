45b276f33621a0373bb98ef3fba8d974918ad217
M A Razvi <abuzarflw@gmail.com>
2022-05-09 14:02:50 -0700

    Define relationships for User, Collection and ArtPiece

--- models/index.js
+++ models/index.js
@@ -1,3 +1,36 @@
 const User = require('./User');
+const Collection = require("./Collection");
+const ArtPiece = require("./ArtPiece");
+// const sequelize = require
 
-module.exports = { User };
+//User relationships
+User.hasMany(Collection, {
+    foreignKey: "user_id",
+});
+
+User.hasMany(ArtPiece, {
+    foreignKey: "user_id",
+});
+
+//Collection relationships
+Collection.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+Collection.hasMany(ArtPiece, {
+    foreignKey: "collection_id"
+});
+
+//ArtPiece relationships
+ArtPiece.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+ArtPiece.belongsTo(Collection, {
+    foreignKey: "collection_id",
+    onDelete: "SET NULL"
+})
+
+module.exports = { User, Collection, ArtPiece };
