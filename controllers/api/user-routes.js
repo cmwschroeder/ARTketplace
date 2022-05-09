8cf566c9e6f1b7fd09a2e79960cfe4089216d4a0
cmwschroeder <cmwschroeder@gmail.com>
2022-05-09 13:05:42 -0700

    added creating a user as well as logging out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,30 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//route that creates a user
+router.post('/', async (req, res) => {
+    try{
+        //create a user instance in the database
+        const userData = await User.create({
+            name: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+        });
+
+        //save the fact that we are logged in and the user id to session storage
+        req.session.save(() => {
+            req.session.userId = userData.id;
+            req.session.loggedIn = true;
+      
+            res.status(200).json(userData);
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
 //route that handles login
 router.post('/login', async (req, res) => {
     try{
@@ -29,6 +53,17 @@ router.post('/login', async (req, res) => {
         });
     } catch (err) {
         res.status(500).json(err);
+    }
+});
+
+//Route that handles logout
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
     }
 });
 
