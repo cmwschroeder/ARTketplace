f5ba2570107c2476ddcd8c5a3d2f9e279a92519c
M A Razvi <abuzarflw@gmail.com>
2022-05-10 14:24:46 -0700

    Define GET route for all collections page

--- controllers/collections-routes.js
+++ controllers/collections-routes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 const { ArtPiece, Collection, User } = require('../models');
 
-
+//Get route for /collections
 router.get('/', async (req, res) => {
+    try {
+        const allCollections = await ArtPiece.findAll({
+            include: [User, Collection]
+        });
+        const theCollections = allCollections.map((collection) => collection.get({ plain: true }));
+
+        res.render("allCollection", {theCollections, loggedIn: req.session.loggedIn})
+    } catch (error) {
+        res.status(500);
+    }
 
 
 });
