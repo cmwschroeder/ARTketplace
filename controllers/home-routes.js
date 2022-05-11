80e969ff069dbe09ad476672d0ecdbcbcd22adae
Nghia Le <davidle314@hotmail.com>
2022-05-11 03:14:13 -0700

    homeroute added with single search, or click to single view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -13,7 +13,7 @@ router.get('/', async (req, res) => {
                     attributes: ['title', 'user_id', 'description', 'image', 'collection_id', 'is_for_sale', 'price'],
                     include: [{
                             model: Collection,
-                            attributes: ['id', 'title', 'user_id', ],
+                            attributes: ['id', 'title', 'user_id'],
                             include: {
                                 model: User,
                                 attributes: ['id', 'name']
@@ -51,9 +51,43 @@ router.get('/', async (req, res) => {
         })
 
         router.get('/art/:id', async (req, res) => {
+            ArtPiece.findOne({
+                where: {
+                    id: req.params.id
+                },
+                attributes: ['title', 'user_id', 'description', 'image', 'collection_id', 'is_for_sale', 'price'],
+                includes: [
+                    {
+                        model: Collection,
+                        attributes: ['id', 'title', 'user_id'],
+                        includes: {
+                            model: User,
+                            attributes: ['id', 'name']
+                        }
+                    },
+                    {
+                        model: User,
+                        attributes: ['id', 'name']
+                    }
+                ]
+            })
+            .then(artPiecesData => {
+                if (!artPiecesData) {
+                    res.status(404).json({ message: 'No posts found with this id'});
+                    return;
+                }
+                const artPiece = artPiecesData.get ({ plain: true });
 
+                res.render('', {
 
-        })
+                })
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(500).json(err);
+            })
+        });
+        
 
         router.put("/art/:id", async (req, res) => {
 
