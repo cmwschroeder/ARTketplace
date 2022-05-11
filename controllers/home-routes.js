29339dad974166148984949d1aa615fae89b5271
Nghia Le <davidle314@hotmail.com>
2022-05-11 01:03:57 -0700

    home route is finish with asking for user to login and view all the artpiece

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,36 +1,65 @@
 const router = require('express').Router();
-const { ArtPiece, Collection, User } = require('../models');
+const {
+    ArtPiece,
+    Collection,
+    User
+} = require('../models');
 
 router.get('/', async (req, res) => {
-    res.render('homePage', {
-        loggedIn: req.session.loggedIn,
-    });
-})
+            res.render('homePage', {
+                    loggedIn: req.session.loggedIn
+                }),
+                ArtPiece.findAll({
+                    attributes: ['title', 'user_id', 'description', 'image', 'collection_id', 'is_for_sale', 'price'],
+                    include: [{
+                            model: Collection,
+                            attributes: ['id', 'title', 'user_id', ],
+                            include: {
+                                model: User,
+                                attributes: ['id', 'name']
+                            }
+                        },
+                        {
+                            model: User,
+                            attributes: ['id', 'name']
+                        }
+                    ]
+                })
+                .then(artPiecesData => {
+                    const artPosts = artPiecesData.map(artPiece => artPiece.get({
+                        plain: true
+                    }));
+                    res.render('homePage', {
+                        artPosts,
+                        loggedIn: req.session.loggedIn
+                    })
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json(err);
+                });
+            })
+        
 
-router.get('/login', async (req, res) => {
-     if (req.session.logged_in) {
-       res.redirect('/');
-      return;
-     }
-        res.render('login');
+        router.get('/login', async (req, res) => {
+            if (req.session.logged_in) {
+                res.redirect('/');
+                return;
+            }
+            res.render('login');
 
-})
+        })
 
-router.get('/art/:id', async (req, res) => {
-
-
-})
-
-router.put("/art/:id", async (req, res) => {
-
-}),
+        router.get('/art/:id', async (req, res) => {
 
 
+        })
 
+        router.put("/art/:id", async (req, res) => {
 
-module.exports = router;
+        });
 
 
 
 
-
+        module.exports = router;
