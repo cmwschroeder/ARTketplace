0d9bea5b4d5f231c150c15b2fba27c009c3c3110
M A Razvi <abuzarflw@gmail.com>
2022-05-10 18:27:12 -0700

    Add collections to allcollection page

--- controllers/collections-routes.js
+++ controllers/collections-routes.js
@@ -4,15 +4,24 @@ const { ArtPiece, Collection, User } = require('../models');
 //Get route for /collections
 router.get('/', async (req, res) => {
     try {
-        const allCollections = await ArtPiece.findAll({
+        const allArts = await ArtPiece.findAll({
             include: [User, Collection]
         });
-        const theCollections = allCollections.map((collection) => collection.get({ plain: true }));
+        const theArtworks = allArts.map((art) => art.get({ plain: true }));
 
-        res.render("allCollection", {theCollections, loggedIn: req.session.loggedIn})
+        const allCollections = await Collection.findAll({
+            include: [ArtPiece]
+        })
+        const theCollections = allCollections.map((collection) => collection.get({plain: true}));
+    
+
+    res.render("allCollection", {theArtworks, theCollections, loggedIn: req.session.loggedIn});
+
     } catch (error) {
         res.status(500);
     }
+
+        
 
 
 });
