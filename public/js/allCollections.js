83abeacbf785574e6fb7c59af2710be5159bd078
M A Razvi <abuzarflw@gmail.com>
2022-05-11 03:01:04 -0700

    Add event listeners to buttons. Have them redirect to relevant specific collection page

--- public/js/allCollections.js
+++ public/js/allCollections.js
@@ -1,9 +1,12 @@
+//Function that displays collections as carousels
+var collections;
+
 async function displayCollections() {
   //Grab json data sent out by api request
   const response = await fetch("/api/collections", {
     method: "GET",
   });
-  const collections = await response.json();
+  collections = await response.json();
   console.log(collections);
 
   //initialize empty html string
@@ -11,8 +14,8 @@ async function displayCollections() {
 
   //Nested for loop to make collections and populate artworks that belong to each collection
   for (let i = 0; i < collections.length; i++) {
-      if(collections[i].artPieces.length) {
-    generatedString += `
+    if (collections[i].artPieces.length) {
+      generatedString += `
     <h1 class="card-title ml-4 text-accent" style="font-size:x-large; text-transform: uppercase">${
       collections[i].title
     }</h1>
@@ -22,8 +25,8 @@ async function displayCollections() {
       
       <div class="owl-carousel owl-theme" id="carousel${i + 1}">`;
 
-    for (let x = 0; x < collections[i].artPieces.length; x++) {
-      generatedString += `
+      for (let x = 0; x < collections[i].artPieces.length; x++) {
+        generatedString += `
         
         
         <div class="item">
@@ -37,22 +40,21 @@ async function displayCollections() {
         </div>
         </div> 
         `;
-    }
-    generatedString += `
+      }
+      generatedString += `
         </div>
       </div>
         <div>  
-            <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg" id="${collections[i].id}" style="margin-bottom: 1vw; margin-left:1vw">View Collection</button>
+            <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg viewCollectionButton" id="${collections[i].id}" style="margin-bottom: 1vw; margin-left:1vw">View Collection</button>
         </div>
       </div>
     </div>
       `;
-  }
+    }
 
-  //Append generated string to the collections page
-  $("#addCarouselHere").append().html(generatedString);
+    //Append generated string to the collections page
+    $("#addCarouselHere").append().html(generatedString);
 
-  
     $(`.owl-carousel`).owlCarousel({
       loop: true,
       margin: 40,
@@ -72,6 +74,15 @@ async function displayCollections() {
           items: 3,
         },
       },
+    });
+  }
+
+  //Add event listeners to view collection buttons
+  var allButtons = document.querySelectorAll(".viewCollectionButton");
+
+  for (let i = 0; i < allButtons.length; i++) {
+    allButtons[i].addEventListener("click", function () {
+      location.href = `/collection/:${collections[i].id}`;
     });
   }
 }
