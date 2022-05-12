41ddf5e3097aac040262fe1b5a24f21bcef9d529
cmwschroeder <cmwschroeder@gmail.com>
2022-05-12 14:40:32 -0700

    on clicking a view button, you're sent to the /art/:id route

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,31 +1,39 @@
 // listen to filter option and get the route data
 const artfilter = document.querySelector(".filter-price")
 artfilter.addEventListener('change', (event) => {
-    const filtervalue = artfilter.value
-    let max 
-    let min 
+    const filtervalue = artfilter.value;
+    let max;
+    let min;
     if (filtervalue === '$50-$200'){
-        max = 200
-        min = 50
+        max = 200;
+        min = 50;
     } else if (filtervalue === '$200-$400'){
-        max = 400
-        min = 200
+        max = 400;
+        min = 200;
     } else if (filtervalue === '$400-$800'){
-        max = 800
-        min = 400
-    } 
+        max = 800;
+        min = 400;
+    };
     if (max && min) {
-        document.location.replace('/filter/'+max +'/' +min)
+        document.location.replace('/filter/'+max +'/' +min);
         
-    }
-})
+    };
+});
 
 const artSort = document.querySelector('.sort-price')
 artSort.addEventListener('change', (event) => {
-    const sortValue = artSort.value
+    const sortValue = artSort.value;
     if (sortValue === 'Lower to Higher') {
-        document.location.replace('/sort/low')
+        document.location.replace('/sort/low');
     } else if (sortValue === 'Higher to Lower') {
-        document.location.replace('/sort/high')
-    }
-})
+        document.location.replace('/sort/high');
+    };
+});
+
+document.querySelectorAll(".edit-button").forEach((button) => {
+    button.addEventListener('click', (event) => {
+        artId = event.target.getAttribute("data-number");
+        console.log(artId);
+        document.location.replace('/art/' + artId);
+    });
+});
