b3bb8ece183def3e94961882802254b9d4e07954
Nghia Le <davidle314@hotmail.com>
2022-05-12 15:24:46 -0700

    added comment for each function

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -3,11 +3,12 @@ const artfilter = document.querySelector(".filter-price");
 const artSort = document.querySelector('.sort-price');
 const artSearch = document.querySelector('#search-button');
 
-
+// event listen for filter option to be click
 artfilter.addEventListener('change', (event) => {
     const filtervalue = artfilter.value;
     let max;
     let min;
+    // if the user click onto one of these options it will show them with the price range they selected
     if (filtervalue === '$50-$200'){
         max = 200;
         min = 50;
@@ -18,16 +19,19 @@ artfilter.addEventListener('change', (event) => {
         max = 800;
         min = 400;
     };
+    // redirecting to a different route
     if (max && min) {
         document.location.replace('/filter/'+max +'/' +min);
         
     };
 });
-
+// event listen for sort option
 artSort.addEventListener('change', (event) => {
     const sortValue = artSort.value;
+    // redirect to a sort route from low to high
     if (sortValue === 'Lower to Higher') {
         document.location.replace('/sort/low');
+         // redirect to a sort route from high to low
     } else if (sortValue === 'Higher to Lower') {
         document.location.replace('/sort/high');
     };
