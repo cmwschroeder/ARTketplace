a0565c788a89bb9cc513c07780006f1b92bd6970
M A Razvi <abuzarflw@gmail.com>
2022-05-12 12:58:45 -0700

    Display confetti when user makes a purchase

--- public/js/oneArt.js
+++ public/js/oneArt.js
@@ -1,15 +1,33 @@
-const buyNow = document.querySelector('#buy-btn');
 
-const buyArt = async (event) => {
-    console.log('Button was clicked!-----------------------------------------------------');
-    const response = await fetch(window.location.pathname, {
-        method: 'PUT'
-    })
-    if(response.ok){
-        alert("Congratualations! It's yours!")
-    }else{
-        alert("Sorry, Failed to Purchase.")
+const confettiCanvas = document.querySelector("#my-canvas");
+const buyNow = document.querySelector(".enabledButton");
+
+var confettiSettings = { target: "my-canvas" };
+var confetti = new ConfettiGenerator(confettiSettings);
+
+
+
+
+buyNow.addEventListener("click", async function() {
+    try {
+        
+        const response = await fetch(window.location.pathname, {
+            method: "PUT",
+          });
+
+          if(response.ok) {
+              
+            
+            confettiCanvas.setAttribute("style", "position: fixed; top:0; left:0; width:100%; height: 100vh; z-index: 1000; visibility: visible; pointer-events: none;")
+              confetti.render();
+              
+          }
+          else {
+              alert("Nopes");
+          }
+    } catch (error) {
+        res.status(500);
     }
-}
+});
 
-buyNow.addEventListener('click', buyArt);
+
