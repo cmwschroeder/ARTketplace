7950ce2cab23734096e7e50895489802a219f0c5
Maya Morales <mayainomorales@gmail.com>
2022-05-11 21:07:06 -0700

    re-pushing

--- controllers/art-routes.js
+++ controllers/art-routes.js
@@ -4,12 +4,14 @@ const sequelize = require("../config/connection");
 
 router.get('/:id', async (req, res) => {
     try {
-        const artPiece = await ArtPiece.findOne({
+        console.log("yoooooooooooooooooooooo---------------------------------------");
+        const theArtPiece = await ArtPiece.findOne({
             where: {id: req.params.id},
             include: [User,Collection]
         })
-        artPiece = artPiece.get({plain:true});
-        res.render('buyArtpage', {theArtPiece, loggedIn: req.session.loggedIn})
+        const artPiece = theArtPiece.get({plain:true});
+        console.log("------------------------------------" + artPiece + "-----------------------------------------------");
+        res.render('buyArtpage', {artPiece, loggedIn: req.session.loggedIn})
     } catch (error) {
       res.status(500);
     }
--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,15 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
 const collectionRoutes = require('./collections-routes');
-const profileRoutes = require('./profile-routes.js')
+const profileRoutes = require('./profile-routes.js');
+const artRoutes = require('./art-routes.js');
 
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/collections', collectionRoutes);
 router.use('/profile', profileRoutes);
+router.use('/art', artRoutes);
 
 
 
