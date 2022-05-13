2efb3ada90e1bd9afbb8f91a76d97a8491069d69
cmwschroeder <cmwschroeder@gmail.com>
2022-05-13 12:01:00 -0700

    added comments to some routes

--- controllers/api/collection-routes.js
+++ controllers/api/collection-routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const { User, Collection, ArtPiece } = require('../../models');
 
+//Gets and returns all the collections that a user owns
 router.get('/user/:id', async (req,res) => {
+    //find all the collections this user owns based off the id of the user passed in
     const collectionData = await Collection.findAll({
         where: {user_id: req.params.id},
         include: [ArtPiece],
@@ -9,6 +11,7 @@ router.get('/user/:id', async (req,res) => {
 
     const collections = collectionData.map((collection) => collection.get({ plain: true }));
 
+    //return a list of the collections
     res.json(collections);
 });
 
--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -1,13 +1,16 @@
 const router = require('express').Router();
 const { ArtPiece, Collection, User } = require('../models');
 
+//Route that will render the profile page
 router.get('/', async (req, res) => {
+    //find all the art pieces the user owns so we can load the initial handlebar with those art pieces
     const artData = await ArtPiece.findAll({
         where: {user_id: req.session.userId}
     });
 
     const artPieces = artData.map((art) => art.get({ plain: true}));
 
+    //render the page with the art pieces
     res.render('profile', {
         loggedIn: req.session.loggedIn,
         artPieces: artPieces,
